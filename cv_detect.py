cbb4afaf56feb733273729390ed1a66802670002
huan-nguyen <huan2931@gamil.com>
2023-03-09 22:43:54 +0700

    change update to set

--- cv_detect.py
+++ cv_detect.py
@@ -159,7 +159,7 @@ def connect_to_firebase():
 #***********************************************************************
 def post_to_firebaserealtime(OOS,In_stock,avalible,img_name):
     ref = db.reference('/')
-    ref.update({
+    ref.set({
         'Shelf' : {
             'OOS' : OOS,
             'In-stock' : In_stock,
